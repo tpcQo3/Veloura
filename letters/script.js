f3ca6aaefd62a28cf4dc4fc3ea4f0cb1689935a5
An Binh <lunarclientqt3333@gmail.com>
2026-04-03 23:40:03 +0700

    ab

--- letters/script.js
+++ letters/script.js
@@ -2,19 +2,35 @@ function parseMarkdown(text) {
   return text
     .replace(/\*\*(.*?)\*\*/g, "<b>$1</b>")
     .replace(/\*(.*?)\*/g, "<i>$1</i>")
+    .replace(/__(.*?)__/g, "<u>$1</u>")
     .replace(/\n/g, "<br>");
 }
 
 function updatePreview() {
   const content = document.getElementById("content").value;
   const theme = document.getElementById("theme").value;
-  const font = document.getElementById("font").value;
+  const preview = document.getElementById("preview");
 
-  const preview = document.getElementById("previewBox");
-
-  preview.className = "";
-  preview.classList.add(theme);
-  preview.classList.add("font-" + font);
-
+  preview.className = theme;
   preview.innerHTML = parseMarkdown(content);
+
+  document.getElementById("count").innerText =
+    content.length + " / 2000";
+}
+
+function wrap(symbol) {
+  const textarea = document.getElementById("content");
+  const start = textarea.selectionStart;
+  const end = textarea.selectionEnd;
+
+  const text = textarea.value;
+
+  textarea.value =
+    text.substring(0, start) +
+    symbol +
+    text.substring(start, end) +
+    symbol +
+    text.substring(end);
+
+  updatePreview();
 }
